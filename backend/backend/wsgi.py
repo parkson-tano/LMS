4e9d93d78c079e23a0807f105ca2b44d03de4561
parkson-tano <tanoparksonsilencer@gmail.com>
2023-01-10 03:26:43 +0100

    deploying to railway

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+app = application
